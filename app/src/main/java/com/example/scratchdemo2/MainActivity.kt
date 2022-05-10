2864aa4c5ece43b6c18f38787d2a8165d44922fb
VivM1 <mevivekmi112@gmail.com>
2022-05-10 20:58:20 +0530

    dependencies are set

---
+++ app/src/main/java/com/example/scratchdemo2/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.scratchdemo2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //material library karni h sahi
+    }
+}
